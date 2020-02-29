81e2e855e3a6b7ca21dcad1361a0f778e0473e25
Lucy Linder <lucy.derlin@gmail.com>
2020-02-29 19:26:54 +0100

    fix errors for pypi upload

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 import setuptools
+import os
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
 
 setuptools.setup(
     name='get-html',
-    version='0.1.0',
+    version='0.0.2',
     author='Lucy Linder',
     author_email='lucy.derlin@gmail.com',
     description='HTTP GET with JS rendering support, to get the rendered HTML from a page easily',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='Apache License 2.0',
-    url='https://github.com/derlin/http-get',
+    url='https://github.com/derlin/get-html',
 
     packages=setuptools.find_packages(),
-    
+
     python_requires='>=3.6',
 
     classifiers=(
         'Programming Language :: Python :: 3',
-        'License :: Apache License 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ),
     install_requires=[
